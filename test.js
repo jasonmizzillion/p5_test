c063951161cd49631a051a171edecca763091aa9
jasonmizzillion <jason.mizzelle@pcc.edu>
2019-01-23 23:05:40 -0800

    newshtuff

--- test.js
+++ test.js
@@ -5,8 +5,16 @@ function setup() {
 function draw() {
 	background(300, 300, 25);
 beginShape();
+  fill(100,20,20);
   vertex(80, 100);
   quadraticVertex(207, 53, 164, 154);
   bezierVertex(391, 384, 214, 137, 130, 310);
 endShape(CLOSE)
+beginShape();
+  fill(200,20,155);
+  vertex(139, 102);
+  quadraticVertex(225, 32, 270, 120);
+  bezierVertex(369, 209, 379, 35, 319, 244);
+endShape(CLOSE)
+
 }
